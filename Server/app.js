93457fab55bc7612a355ad7b88fe52f3c15ebdd0
aryansingh144 <aryansingh7910@gmail.com>
2024-11-19 20:30:48 +0530

    running

--- Server/app.js
+++ Server/app.js
@@ -1,13 +1,13 @@
 import express from "express"
 import helmet from "helmet"
-import auth from './src/routes/userRoutes';
+// import auth from './src/routes/userRoutes';
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 
-app.use('/',auth);
+// app.use('/',auth);
 
 export default app;
