337178df7266e25450344054b9ffdeaf7f9b2497
RAJAT KUMAR <rajat@RAJATs-MacBook-Air.local>
2024-11-20 13:46:30 +0530

    rajatfirstcommit

--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import helmet from "helmet"
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js"
+import  foodListingRoutes from "./routes/foodListingRoutes.js"
 const app = express();
 
 app.use(helmet());
@@ -12,5 +13,6 @@ app.use(cors());
 // app.use('/',auth);
 
 app.use("/api", userRoutes);
+app.use("/api/listings",foodListingRoutes);
 
 export default app;
